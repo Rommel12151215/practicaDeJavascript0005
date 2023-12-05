2d80fdef6b004ad54210fb7c9b93c5093f4b3325
Rommel <karlsrommel255@gmail.com>
2023-12-04 22:11:44 -0600

    practicaDeJavascript0005

---
+++ index.js
@@ -0,0 +1,5 @@
+function changeBackgroundColor() {
+    var randomColor = '#' + Math.floor(Math.random()*16777215).toString(16);
+    document.body.style.backgroundColor = randomColor;
+}
+setInterval(changeBackgroundColor, 1000);
